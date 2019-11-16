6be7dcc2c729a9884e9e8552fae71ae6fc4f07d3
Shawn Lim <shawn.lim@local>
2019-11-16 18:47:24 +0800

    Use enum and pattern matching for generic area calculation

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum Shape {
+    Rectangle { width: f64, height: f64 },
+    Square(f64),
+    Circle(f64),
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, rect: &Rectangle) -> bool {
-        self.width >= rect.width && self.height >= rect.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle { width: size, height: size }
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Rectangle { width, height } => width * height,
+            Shape::Square(size) => size * size,
+            Shape::Circle(radius) => std::f64::consts::PI * (radius * radius),
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
-    let rect4 = Rectangle::square(10);
+    let shape1 = Shape::Rectangle { width: 30.0, height: 50.0 };
+    let shape2 = Shape::Square(10.0);
+    let shape3 = Shape::Circle(5.0);
     
-    println!("rect1 is {:?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!("The area of the rectangle is {} square pixels.", rect4.area());
+    println!("shape1 is {:?}", shape1);
+    println!("shape2 is {:?}", shape2);
+    println!("shape3 is {:?}", shape3);
+    println!("The area of the shape1 is {} square pixels.", shape1.area());
+    println!("The area of the shape2 is {} square pixels.", shape2.area());
+    println!("The area of the shape3 is {} square pixels.", shape3.area());
 }
