7d8f1658a413f583ea62f83d9f1213e9740f5c5e
Shawn Lim <shawn.lim@local>
2019-11-17 11:52:02 +0800

    Break up main into modules

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
-#[derive(Debug)]
-enum Shape {
-    Rectangle { width: f64, height: f64 },
-    Square(f64),
-    Circle(f64),
-    Triangle { base: f64, height: f64 },
-}
-
-impl Shape {
-    fn area(&self) -> f64 {
-        match self {
-            Shape::Rectangle { width, height } => width * height,
-            Shape::Square(size) => size * size,
-            Shape::Circle(radius) => std::f64::consts::PI * (radius * radius),
-            Shape::Triangle { base, height } => 0.5 * base * height,
-        }
-    }
-}
+use learn_rust::shape::Shape;
 
 fn main() {
     let shape1 = Shape::Rectangle { width: 30.0, height: 50.0 };
